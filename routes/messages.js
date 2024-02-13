5a07e31873b5c3d6b3180f238672378190f7a817
Your Name <you@example.com>
2024-02-13 00:35:05 -0800

    get messsages, post msg route

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,12 @@
+const Router = require("express").Router;
+const Message = require("../models/message");
+const {ensureLoggedIn, ensureCorrectUser} = require("../middleware/auth");
+const ExpressError = require("../expressError");
+
+const router = new Router();
+
+
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -10,7 +19,21 @@
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get('/:id', ensureLoggedIn, async (req, res, next) => {
+    try{ 
+        let username = req.user.username;
+        let msg = await Message.get(req.params.id);
 
+        if (msg.to_user.username !== username && msg.from_user.username !== username) {
+            throw new ExpressError("Cannot read this message", 401);
+        }
+
+        return res.json({message: msg});
+    }
+    catch(err){
+        return next(err);
+    }
+} )
 
 /** POST / - post message.
  *
@@ -18,7 +41,20 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
-
+router.post('/', ensureLoggedIn, async (req, res, next) => {
+    try{
+        let body = req.body.body;
+        let from_username = req.user.username;
+        let to_username = req.body.to_username
+        
+        let msg = await Message.create( from_username, to_username, body )
+        return res.json({message: msg});
+    
+    }
+    catch(err){
+        return next(err);
+    }
+})
 
 /** POST/:id/read - mark message as read:
  *
