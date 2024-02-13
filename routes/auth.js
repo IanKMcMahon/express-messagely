c4f59b4968cc6cce33f47c105473eeb752889058
Your Name <you@example.com>
2024-02-12 23:06:58 -0800

    login Post route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,31 @@
-/** POST /login - login: {username, password} => {token}
- *
- * Make sure to update their last-login!
- *
- **/
+const jwt = require("jsonwebtoken");
+const Router = require("express").Router;
+const router = new Router();
+
+const User = require("../models/user");
+const {SECRET_KEY} = require("../config");
+const ExpressError = require("../expressError");
 
 
+
+
+//POST /login - login: {username, password} => {token}
+router.post('/login', async (req, res, err) => {
+    try{
+        let {username, password} = req.body;
+        if (await User.authenticate(username, password)) {
+            let token = jwt.sign({username}, SECRET_KEY);
+            User.updateLoginTimestamp(username);
+            return res.json({token});
+        }
+        else {
+            throw new ExpressError("Invalid Username/Password", 400);
+        }
+    }
+    catch(err){
+        return next(err)
+    }
+}) 
 /** POST /register - register user: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
