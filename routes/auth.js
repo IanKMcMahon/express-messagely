7ec0cce5eb4aa59c5578c8334be61e051aeeeb1c
Your Name <you@example.com>
2024-02-12 23:19:50 -0800

    register Post route

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ const ExpressError = require("../expressError");
 
 
 //POST /login - login: {username, password} => {token}
-router.post('/login', async (req, res, err) => {
+router.post('/login', async (req, res, next) => {
     try{
         let {username, password} = req.body;
         if (await User.authenticate(username, password)) {
@@ -26,9 +26,20 @@ router.post('/login', async (req, res, err) => {
         return next(err)
     }
 }) 
-/** POST /register - register user: registers, logs in, and returns token.
- *
- * {username, password, first_name, last_name, phone} => {token}.
- *
- *  Make sure to update their last-login!
- */
+//  register user: registers, logs in, and returns token.
+//  {username, password, first_name, last_name, phone} => {token}.
+
+router.post('/register', async (req, res, next) => {
+    try{
+        let { username } = await User.register(req.body);
+        let token = jwt.sign({username}, SECRET_KEY);
+        User.updateLoginTimestamp(username);
+        return res.json({token});
+    }
+    catch(err){
+        return next(err);        
+    }
+});
+
+module.exports = router;
+
