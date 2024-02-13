b7e8ac2f103d2766d4e50347c9949f4a96395c29
Your Name <you@example.com>
2024-02-12 21:38:15 -0800

    messagesFrom

--- models/user.js
+++ models/user.js
@@ -66,7 +66,7 @@ class User {
    * [{username, first_name, last_name, phone}, ...] */
 
   static async all() {
-    const results = db.query(
+    const results = await db.query(
       `SELECT 
       username, 
       first_name,
@@ -87,7 +87,23 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const results = await db.query(
+      `SELECT username,
+        first_name,
+        last_name,
+        phone,
+        join_at,
+        last_login_at
+      FROM users
+      WHERE username = $1`,
+      [username]
+    )
+    if (!results.rows[0]) {
+      throw new ExpressError(`No such user: ${username}`, 404) 
+    }
+    else return results.rows[0];
+   }
 
   /** Return messages from this user.
    *
@@ -97,7 +113,34 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {
+    const result = await db.query(
+        `SELECT m.id,
+                m.to_username,
+                u.first_name,
+                u.last_name,
+                u.phone,
+                m.body,
+                m.sent_at,
+                m.read_at
+          FROM messages AS m
+            JOIN users AS u ON m.to_username = u.username
+          WHERE from_username = $1`,
+        [username]);
+
+    return result.rows.map(m => ({
+      id: m.id,
+      to_user: {
+        username: m.to_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at
+    }));
+  }
 
   /** Return messages to this user.
    *
