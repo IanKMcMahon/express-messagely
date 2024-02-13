c5b9886ccd3c1a600aa643a9703363d204a87563
Your Name <you@example.com>
2024-02-12 23:41:07 -0800

    cleanup

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,8 @@ const router = new Router();
 
 
 
-/** GET / - get list of users.
- *
- * => {users: [{username, first_name, last_name, phone}, ...]}
- *
- **/
+// GET / - get list of users.
+
 router.get('/', ensureLoggedIn, async (req, res, next) => {
     try{ 
         let users = await User.all()
@@ -21,11 +18,8 @@ router.get('/', ensureLoggedIn, async (req, res, next) => {
     }
 })
 
-/** GET /:username - get detail of users.
- *
- * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
- *
- **/
+// GET /:username - get detail of users.
+
 router.get('/:username', ensureCorrectUser, async (req, res, next) => {
     try{ 
         let user = await User.get(req.params.username);
@@ -36,15 +30,8 @@ router.get('/:username', ensureCorrectUser, async (req, res, next) => {
     }
 })
 
-/** GET /:username/to - get messages to user
- *
- * => {messages: [{id,
- *                 body,
- *                 sent_at,
- *                 read_at,
- *                 from_user: {username, first_name, last_name, phone}}, ...]}
- *
- **/
+// GET /:username/to - get messages to user
+
 router.get('/:username/to', ensureCorrectUser, async (req, res, next) => {
     try{ 
         let messages = await User.messagesTo(req.params.username);
@@ -55,15 +42,8 @@ router.get('/:username/to', ensureCorrectUser, async (req, res, next) => {
     }
 })
 
-/** GET /:username/from - get messages from user
- *
- * => {messages: [{id,
- *                 body,
- *                 sent_at,
- *                 read_at,
- *                 to_user: {username, first_name, last_name, phone}}, ...]}
- *
- **/
+// GET /:username/from - get messages from user
+
 router.get('/:username/from', ensureCorrectUser, async (req, res, next) => {
     try{ 
         let messages = await User.messagesFrom(req.params.username);
